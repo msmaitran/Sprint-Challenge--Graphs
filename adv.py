f2ec7a482b0350487ff7068121cb2aecd72a6ed3
msmaitran <itsmaisn@gmail.com>
2020-04-17 10:17:19 -0700

    Add solution for adv.py

--- adv.py
+++ adv.py
@@ -29,7 +29,30 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+previous_direction = []
+visited = {}
+visited[0] = player.current_room.get_exits()
 
+reverse_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+# While the number of rooms visited is less than the total rooms
+while len(visited) < len(room_graph)-1:
+    # If current room not in visited
+    if player.current_room.id not in visited:
+        # Get exit directions
+        visited[player.current_room.id] = player.current_room.get_exits()
+        # Remove previous direction
+        visited[player.current_room.id].remove(previous_direction[-1])
+    
+    while len(visited[player.current_room.id]) < 1:
+        last = previous_direction.pop()
+        traversal_path.append(last)
+        player.travel(last)
+
+    exit_direction = visited[player.current_room.id].pop(0)
+    traversal_path.append(exit_direction)
+    previous_direction.append(reverse_directions[exit_direction])
+    player.travel(exit_direction)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +74,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
